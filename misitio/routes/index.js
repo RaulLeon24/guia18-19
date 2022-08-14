39f08aaeb8f836b47bf5cdaade6a7acea112f4d1
RaulLeon24 <raulemoran@espol.edu.ec>
2022-08-14 14:33:54 -0500

    backend

---
+++ misitio/routes/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const Producto = require("../models").producto;
+
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/productos", function (req, res, next) {
+  	
+  Producto.findAll({  
+    attributes: { exclude: ["updatedAt"] }  
+})  
+.then(productos => {  
+    res.render('productos', { title: 'My Dashboard :: Productos', arrProductos: productos });  
+})  
+.catch(error => res.status(400).send(error)) 
+});
+
+module.exports = router;
